4c2047b19d3ed4279e8e76b081787bf65857244c
“kieuvu” <kieuvuvuvu@gmail.com>
2023-05-15 00:24:38 +0900

    get todo list by user

--- src/modules/todo/TodoModule.ts
+++ src/modules/todo/TodoModule.ts
@@ -3,10 +3,12 @@ import { TodoService } from './TodoService';
 import { CreateTodoAction } from './actions/CreateTodoAction';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoEntity } from './TodoEntity';
+import { GetListTodoAction } from './actions/GetListTodoAction';
+import { UserModule } from '../user/UserModule';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TodoEntity])],
+  imports: [TypeOrmModule.forFeature([TodoEntity]), UserModule],
   providers: [TodoService],
-  controllers: [CreateTodoAction],
+  controllers: [CreateTodoAction, GetListTodoAction],
 })
 export class TodoModule {}
