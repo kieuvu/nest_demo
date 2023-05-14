0369d0845f6819630d639cb6185e7c4e688918f0
“kieuvu” <kieuvuvuvu@gmail.com>
2023-05-15 00:22:36 +0900

    add new http message

--- src/common/utils/Enum.ts
+++ src/common/utils/Enum.ts
@@ -2,4 +2,5 @@ export enum HttpMessage {
   EMAIL_EXISTED = 'Email is already in use',
   FORBIDDEN = 'Forbidden',
   USER_NOT_FOUND = 'User Not Found In Database',
+  INTERNAL_SERVER_ERROR = 'Internal Server Error',
 }
